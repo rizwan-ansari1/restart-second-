f94c172e1905283db9c79a00341f74b74e23820d
curious-tec <reezwanansari444@gmail.com>
2025-09-21 16:29:09 +0545

    first commit

---
+++ b.js
@@ -0,0 +1,11 @@
+// a.js
+
+// This function changes the inner text of para1
+function changeText() {
+  document.getElementById("para1").innerHTML = "Hello JavaScript!";
+}
+
+// This function changes the color of para1
+function changeColor() {
+  document.getElementById("para1").style.color = "orange";
+}
